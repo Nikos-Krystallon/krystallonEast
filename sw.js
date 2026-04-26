a21ac5881b360d368bcc7fe41484d871c2f75ed1
Nikos-Krystallon <krystallonvillas@gmail.com>
2026-04-27 00:38:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'krystallon-1777237951417';
+const CACHE = 'krystallon-1777239265624';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
